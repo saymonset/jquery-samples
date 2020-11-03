1ace4e730ddf03a35fa94385563841763e7d5ecb
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2020-11-03 13:52:18 -0400

    Seccion 5 jquery plugin Slideshow to plugin

--- js/slideShow.js
+++ js/slideShow.js
@@ -5,21 +5,21 @@
 
 	var $slideshow = $(".slideShow ul");
 	var slides  = $slideshow.find("li").length;
+	var $puntos = $(".slideShowButtons");
+
+	$puntos.find("div").eq(0).css({
+		backgroundColor: "#58167d"
+	});
 
 	var intervalo = setInterval(function(){
-
-		mover("evento");
-
+		mover("sig");
 	}, 1500 );
 
 
 	function mover( dir, click ){
 
-		if( click )
-			clearInterval( intervalo );
 
-
-		( dir === "evento" ) ? actual-- : actual++;
+		( dir === "sig" ) ? actual-- : actual++;
 
 		if( actual > 0 ){
 
@@ -30,11 +30,27 @@
 		}
 
 
-		var margen = actual * ancho;
+		mover_por_punto( actual, click );
 
+	}
+
+
+	function mover_por_punto( actual, click ){
+
+		if( click )
+			clearInterval( intervalo );
+
+
+		var margen = actual * ancho;
+		var idx    = actual * -1;
+
+		var $puntoActual = $puntos.find("div").eq( idx );
+		var $demasPuntos = $puntos.find("div").not( $puntoActual );
 
 		var tl = new TimelineMax();
-		tl.to( $slideshow, 1.2, { marginLeft: margen, ease: Elastic.easeOut.config(1, 0.75) } );
+		tl.to( $slideshow, 1.2, { marginLeft: margen, ease: Elastic.easeOut.config(1, 0.75) } )
+		  .to( $puntoActual, 0.5, { backgroundColor: "#58167d" }, "-=1.2" )
+		  .to( $demasPuntos, 0.5, { backgroundColor: "#a1a1a1" }, "-=1.2" );
 
 		// $slideshow.animate({
 		// 	marginLeft: margen
@@ -45,10 +61,22 @@
 
 
 
+	$(".slideButton").on("click",function(){
 
-	$(".btn").on("click",function(){
+		var idx =$(this).data("idx");
+		idx = idx *-1;
 
-		var dir = $(this).data("dir");
+		console.log( idx );
+
+		mover_por_punto( idx, true );
+
+	});
+
+
+
+	$(".botSlide").on("click",function(){
+
+		var dir = $(this).data("mov");
 		mover( dir, true );
 
 	});
