48cef347b8ac776ebb8e526886abb92a9cb2092d
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2020-11-03 14:58:00 -0400

    Seccion 5: Slideshow a plugin parte 2-> Caputulo 25 Finshed!

--- js/slideShow.js
+++ js/slideShow.js
@@ -1,85 +1,135 @@
 (function(){
 
-	var actual = 0;
-	var ancho  = 600;
+	$slideShow = function(opciones){
 
-	var $slideshow = $(".slideShow ul");
-	var slides  = $slideshow.find("li").length;
-	var $puntos = $(".slideShowButtons");
+        opciones = $.extend({
+			divDestino: ".slideShow",
+			intervalo: 1500,
+			slides: [],
+			ancho: 600,
+			velocidad: 1200,
+			color: "#ba007c"
+		}, opciones);
 
-	$puntos.find("div").eq(0).css({
-		backgroundColor: "#58167d"
-	});
+		if (opciones.slides.length == 0){
+			alert('Los slides son necesarios.');
+			return;
+		}
 
-	var intervalo = setInterval(function(){
-		mover("sig");
-	}, 1500 );
+		opciones.velocidad = opciones.velocidad / 1000;
+
+		var actual = 0;
+		var ancho  = opciones.ancho;
 
 
-	function mover( dir, click ){
+		var slides  = opciones.slides.length;
 
 
-		( dir === "sig" ) ? actual-- : actual++;
+		//Creciones del slideShow
+		
+		var contenido = "";
+			contenido +="<ul>"
+			  for (var i = 0; i < opciones.slides.length; i++ ){
+                 contenido += '<li><img src="'+ opciones.slides[i] + '"></li>'
+			  }
+			contenido += "</ul>";
 
-		if( actual > 0 ){
+			$(opciones.divDestino).append(contenido);
 
-			actual = ( slides - 1 ) * -1;
+			var $slideshow = $(opciones.divDestino +" ul");
 
-		}else if( actual <= ( slides * -1 ) ){
-			actual = 0;
+			//Creacion de los puntos
+			contenido = "";
+			contenido +='<div class="slideShowButtons">';
+			for(var i = 0; i < opciones.slides.length; i ++){
+                       contenido +='  <div data-idx="' + i + '" class="slideButton"></div>'
+			}
+			contenido +='</div>';
+
+			$(opciones.divDestino).append(contenido);
+
+		var $puntos = $(".slideShowButtons");
+
+		$puntos.find("div").eq(0).css({
+			backgroundColor: opciones.color
+		});
+
+ 
+
+		var intervalo = setInterval(function(){
+			mover("sig");
+		}, opciones.intervalo );
+
+
+		function mover( dir, click ){
+
+
+			( dir === "sig" ) ? actual-- : actual++;
+
+			if( actual > 0 ){
+
+				actual = ( slides - 1 ) * -1;
+
+			}else if( actual <= ( slides * -1 ) ){
+				actual = 0;
+			}
+
+
+			mover_por_punto( actual, click );
+
 		}
 
 
-		mover_por_punto( actual, click );
+		function mover_por_punto( actual, click ){
 
-	}
-
-
-	function mover_por_punto( actual, click ){
-
-		if( click )
-			clearInterval( intervalo );
+			if( click )
+				clearInterval( intervalo );
 
 
-		var margen = actual * ancho;
-		var idx    = actual * -1;
+			var margen = actual * ancho;
+			var idx    = actual * -1;
 
-		var $puntoActual = $puntos.find("div").eq( idx );
-		var $demasPuntos = $puntos.find("div").not( $puntoActual );
+			var $puntoActual = $puntos.find("div").eq( idx );
+			var $demasPuntos = $puntos.find("div").not( $puntoActual );
 
-		var tl = new TimelineMax();
-		tl.to( $slideshow, 1.2, { marginLeft: margen, ease: Elastic.easeOut.config(1, 0.75) } )
-		  .to( $puntoActual, 0.5, { backgroundColor: "#58167d" }, "-=1.2" )
-		  .to( $demasPuntos, 0.5, { backgroundColor: "#a1a1a1" }, "-=1.2" );
+			var tl = new TimelineMax();
+			tl.to( $slideshow, opciones.velocidad, { marginLeft: margen, ease: Elastic.easeOut.config(1, 0.75) } )
+			.to( $puntoActual, 0.5, { backgroundColor: opciones.color }, "-=1.2" )
+			.to( $demasPuntos, 0.5, { backgroundColor: "#a1a1a1" }, "-=1.2" );
 
-		// $slideshow.animate({
-		// 	marginLeft: margen
-		// }, 400 );
+			// $slideshow.animate({
+			// 	marginLeft: margen
+			// }, 400 );
 
 
-	}
+		}
 
 
 
-	$(".slideButton").on("click",function(){
+		$(".slideButton").on("click",function(){
 
-		var idx =$(this).data("idx");
-		idx = idx *-1;
+			var idx =$(this).data("idx");
+			idx = idx *-1;
 
-		console.log( idx );
+			console.log( idx );
 
-		mover_por_punto( idx, true );
+			mover_por_punto( idx, true );
 
-	});
+		});
 
 
 
-	$(".botSlide").on("click",function(){
+		$(".botSlide").on("click",function(){
 
-		var dir = $(this).data("mov");
-		mover( dir, true );
+			var dir = $(this).data("mov");
+			mover( dir, true );
 
-	});
+		});
+
+
+	};
+
+	
 
 
 })();
